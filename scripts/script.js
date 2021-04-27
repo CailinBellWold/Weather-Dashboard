7218c3f6e11a21a21bb76b920999edebff2f2216
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-04-26 23:01:19 -0700

    Started outlining requests.

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,55 @@
 // Variables
-
-// Content Areas
+let citySearchInputEl = document.querySelector('#city-search-input');
+let citySearchInput;
+let apiKey = 'a46fae6c97cce84840e8dfd333cdaca5';
 
 // Button
-const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search-input');
-searchButton.addEventListener('click', () => {
-  const inputValue = searchInput.value;
-  alert(inputValue);
-});
+let searchButton = document.getElementById('search-button');
+
+let searchButtonHandler = function (event) {
+  event.preventDefault();
+
+  citySearchInput = citySearchInputEl.value.trim();
+
+  if (citySearchInput) {
+    getWeatherData(citySearchInput);
+
+    // repoContainerEl.textContent = '';
+    // nameInputEl.value = '';
+  } else {
+    alert('Please enter a City');
+  }
+};
+
+searchButton.addEventListener('click', searchButtonHandler);
+
+fetch(apiURL, {
+  // The browser fetches the resource from the remote server without first looking in the cache.
+  // The browser will then update the cache with the downloaded resource.
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+// Will need this to covert city to a lat/lon
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key} 
+
+
+function getWeatherData (city) {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&appid=' + apiKey;
+  
+    fetch(apiUrl).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayWeather(data.XXXXX, city);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    });
+  };
+
+//GOAL: 1.) Take user input, trim, convert case (if necessary). 2.) Store Input to local storage 3.) Convert city to lat/lon 4.) Run function to retrieve weather. 5.) Input responses into correct spans.  

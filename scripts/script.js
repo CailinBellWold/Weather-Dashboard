98c297c53edda0c118046208d220db0494989311
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-04-26 21:43:06 -0700

    Unpolished Basic Layout.

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,9 @@
 // Content Areas
 
 // Button
-
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+searchButton.addEventListener('click', () => {
+  const inputValue = searchInput.value;
+  alert(inputValue);
+});

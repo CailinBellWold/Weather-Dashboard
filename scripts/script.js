14e7fe0b8d991be5081412e7bf4dc60faf20179c
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-04-28 20:28:23 -0700

    Setting Up Search History Buttons

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@
 let citySearchInputEl = document.querySelector('#city-search-input');
 let citySearchInput;
 let apiKey = 'a46fae6c97cce84840e8dfd333cdaca5';
+let city;
+let latLonApiUrl;
 
 // Button
 let searchButton = document.getElementById('search-button');
@@ -9,13 +11,11 @@ let searchButton = document.getElementById('search-button');
 let searchButtonHandler = function (event) {
   event.preventDefault();
 
-  citySearchInput = citySearchInputEl.value.trim();
+  citySearchInput = citySearchInputEl.value.trim(); //TO DO: How to adjust for space/no space, comma, no comma, etc. Start with City, for now...
 
   if (citySearchInput) {
-    getWeatherData(citySearchInput);
-
-    // repoContainerEl.textContent = '';
-    // nameInputEl.value = '';
+    getLatLon(citySearchInput);
+    setHistory(citySearchInput);
   } else {
     alert('Please enter a City');
   }
@@ -23,20 +23,53 @@ let searchButtonHandler = function (event) {
 
 searchButton.addEventListener('click', searchButtonHandler);
 
-fetch(apiURL, {
-  // The browser fetches the resource from the remote server without first looking in the cache.
-  // The browser will then update the cache with the downloaded resource.
-})
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+function setHistory() {
+  for <i = 0; i < 8; i++> {
+  localStorage.setItem(city-[], citySearchInput);
+  }
+};
 
+function renderHistory() {
+    $("#city-1").val(localStorage.getItem('city-1'));
+    $("#city-2").val(localStorage.getItem('city-2'));
+    $("#city-3").val(localStorage.getItem('city-3'));
+    $("#city-4").val(localStorage.getItem('city-4'));
+    $("#city-5").val(localStorage.getItem('city-5'));
+    $("#city-6").val(localStorage.getItem('city-6'));
+    $("#city-7").val(localStorage.getItem('city-7'));
+    $("#city-8").val(localStorage.getItem('city-8'));
+};
+
+
+// fetch(apiURL, {
+//   // The browser fetches the resource from the remote server without first looking in the cache.
+//   // The browser will then update the cache with the downloaded resource.
+// })
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
+
+//TO DO: Update with State/Country once I get this working.
+  function getLatLon (citySearchInput) {
+    var latLonApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + citySearchInput + "," + state + "," + country + '&limit={limit}&appid=' + apiKey;
+  
+    fetch(latLonApiUrl).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          lat = data.lat;
+          long = data.lon;
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    });
+  };
+  
 // Will need this to covert city to a lat/lon
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key} 
-
 
 function getWeatherData (city) {
     var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&appid=' + apiKey;
